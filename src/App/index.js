a0fc69304bf5fe901e2945d1e16b36699c2d55a5
Ratouney <mfouchardiere@avarteq.de>
2017-08-10 18:22:32 +0200

    Trying out to do a simple request

--- src/App/index.js
+++ src/App/index.js
@@ -4,10 +4,17 @@ import './../App.css';
 import MainPane from './../layout/MainPane';
 import Menus from './../layout/Menus';
 import Note from './../note/Note';
-
+import fetch from 'isomorphic-fetch';
 
 class App extends Component {
+  getReddit() {
+    let stuff = fetch('https://playoverwatch.com/en-gb/career/pc/eu/Ratouney-2516')
+    .then(response => response.json())
+    console.log(stuff)
+  }
+
   render() {
+    this.getReddit()
     return (
       <Layout>
         <Menus />
