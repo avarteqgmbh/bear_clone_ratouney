c64cadb9ae734c8ea646fc261926fc59adf5f08d
Maxime de la Fouchardiere - Ratouney <mfouchardiere@avarteq.de>
2017-08-21 10:27:58 +0200

    Add function works

--- src/notelist/Reducers.js
+++ src/notelist/Reducers.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import {
     SELECT_NOTE,
+    ADD_NOTE,
 } from './Types';
 import {
     UPDATE_TITLE,
@@ -9,21 +11,33 @@ import {
 
 const initialState = {
     notes: [
-        { id: 1, title: "Dank Memes", body: "I love dank memes", category: "GENERAL", owner: "ratouney" },
-        { id: 2, title: "Reddit", body: "Sometimes, i'm bored", category: "TRASH", owner: "ratouney" },
-        { id: 3, title: "Overwatch", body: "The cancer that i love", category: "GENERAL", owner: "ratouney" },
-        { id: 4, title: "Epitech", body: "Blinux is ugly", category: "TRASH", owner: "ratouney" },
-        { id: 5, title: "Stuff", body: "Nothing here", category: "GENERAL", owner: "ratouney" },
-        { id: 6, title: "New Note", body: "Nope", category: "TEMP", owner: "ratouney" },
+        { id: 1, title: 'Dank Memes', body: 'I love dank memes', category: 'GENERAL', owner: 'ratouney' },
+        { id: 4, title: 'Epitech', body: 'Blinux is ugly', category: 'TRASH', owner: 'ratouney' },
+        { id: 6, title: 'New Note', body: 'Nope', category: 'TEMP', owner: 'ratouney' },
+        { id: 3, title: 'Overwatch', body: 'The cancer that i love', category: 'GENERAL', owner: 'ratouney' },
+        { id: 5, title: 'Stuff', body: 'Nothing here', category: 'GENERAL', owner: 'ratouney' },
+        { id: 2, title: 'Reddit', body: 'Sometimes, i\'m bored', category: 'TRASH', owner: 'ratouney' },
     ],
-    categories: ["GENERAL", "TRASH", "TEMP"],
-    selected: -1,
+    categories: ['GENERAL', 'TRASH', 'TEMP'],
+    selected:   -1,
 };
 
 const NoteListReducer = function NoteListReducer(state = initialState, action) {
-    console.log('[NoteListReducer:Action] -', action)
+    console.log('[NoteListReducer:Action] -', action);
 
     switch (action.type) {
+        case ADD_NOTE:
+            const ids = state.notes.map((elem) => elem.id);
+            const new_highest = _.max(ids) + 1;
+            const new_elem = { id: new_highest, title: '', body: '', category: action.category, owner: 'ratouney' };
+            return Object.assign(
+                {},
+                state,
+                {
+                    notes: [...state.notes, new_elem],
+                    selected: new_highest,
+                });
+
         case SELECT_NOTE:
             return { ...state, selected: action.id };
 
--- src/notelist/index.js
+++ src/notelist/index.js
@@ -4,36 +4,36 @@ import { connect } from 'react-redux';
 import { selectNote, addNote } from './Actions';
 
 const DispatchSelector = function DispatchSelector(func_select, func_add, info) {
-    if (info.key === 'add_note') {
-        console.log('Add note to category : ', info.keyPath[1])
-        func_add(info.keyPath[1])
-    } else {
-        func_select(info.key)
-    }
+  if (info.key === 'add_note') {
+    console.log('Add note to category : ', info.keyPath[1]);
+    func_add(info.keyPath[1]);
+  } else {
+    func_select(info.key);
+  }
 }
 
-const RenderNoteList = function RenderNoteList({onClickNote, onAddNote, listName, notes}) {
-    return (
-        <Menu theme="dark" mode="inline" onClick={(e) => DispatchSelector(onClickNote, onAddNote, e)} >
-            <Menu.SubMenu key={listName} title={<span className="nav-text" style={{ fontSize: '16px' }} >{listName}</span>} >
-            {notes.map((note) => (
-                <Menu.Item key={note.id} >{note.title}</Menu.Item>
-            ))}
-            <Menu.Item key={"add_note"}><Icon type="plus-square"/><span style={{ color: 'green' }}> New Note</span></Menu.Item>
-            </Menu.SubMenu>
-        </Menu>
-    )
-}
+const RenderNoteList = function RenderNoteList({ onClickNote, onAddNote, listName, notes }) {
+  return (
+    <Menu theme="dark" mode="inline" onClick={(e) => { DispatchSelector(onClickNote, onAddNote, e); }} >
+      <Menu.SubMenu key={listName} title={<span className="nav-text" style={{ fontSize: '16px' }} >{listName}</span>} >
+        {notes.map((note) => (
+          <Menu.Item key={note.id} >{note.title}</Menu.Item>
+        ))}
+        <Menu.Item key={'add_note'}><Icon type="plus-square" /><span style={{ color: 'green' }}> New Note</span></Menu.Item>
+      </Menu.SubMenu>
+    </Menu>
+  );
+};
 
 function mapDispatchToProps(dispatch) {
-    return {
-        onClickNote: (id) => {
-            dispatch(selectNote(id))
-        },
-        onAddNote: (categ) => {
-            dispatch(addNote(categ))
-        },
-    }
+  return {
+    onClickNote: (id) => {
+      dispatch(selectNote(id));
+    },
+    onAddNote: (categ) => {
+      dispatch(addNote(categ));
+    },
+  };
 }
 
 const NoteList = connect(null, mapDispatchToProps)(RenderNoteList);
